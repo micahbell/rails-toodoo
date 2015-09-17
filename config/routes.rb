e34b13b7825d985225e5236267e4ecbc2ddc24e9
Micah Bell <micahmbell@gmail.com>
2015-09-16 22:30:04 -0600

    Generated todo list scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :todo_lists
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'todo_lists#index'
   # root 'welcome#index'
 
   # Example of regular route:
